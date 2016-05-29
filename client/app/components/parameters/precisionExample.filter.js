365f665aad1a83d562a516a74de1bcaa70738606
BSolomon <bryan.solomon@hpe.com>
2016-05-28 21:06:10 -0500

    Further development on parameters page | outbound box developed | hidden uom conversion developed

---
+++ client/app/components/parameters/precisionExample.filter.js
@@ -0,0 +1,26 @@
+/**
+ * Created by solomonb on 5/28/2016.
+ */
+(function(){
+    "use strict";
+
+    function PrecisionFilter(){
+        return function(input) {
+            if (!angular.isNumber(input)) {
+                if (input && input.length > 0) {
+                    var padding = "";
+                    for (var i = 0; i < input; i++) {
+                        padding += "0";
+                    }
+                    return "0." + padding;
+                } else {
+                    return "";
+                }
+            }
+        }
+    }
+
+    angular.module("DCX")
+           .filter("precisionExample", PrecisionFilter);
+
+})();
